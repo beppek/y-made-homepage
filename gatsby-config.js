7e8c948f2323de032373bceac089521066bf43dc
Beppe Karlsson <beppe@live.se>
2019-01-13 00:40:21 +1100

    Add identity widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    'gatsby-plugin-netlify-identity-widget',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
