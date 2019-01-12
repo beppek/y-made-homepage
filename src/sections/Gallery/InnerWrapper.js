cc57301fc687a6d32b261bd89d828b7789e9d8e1
Beppe Karlsson <beppe@live.se>
2019-01-13 00:10:28 +1100

    Offset with padding

--- src/sections/Gallery/InnerWrapper.js
+++ src/sections/Gallery/InnerWrapper.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
+import { paddingNumber, paddingUnit } from '../../constants/units';
+
 const InnerWrapper = styled.div`
   position: absolute;
   width: 100%;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, calc(-50% + ${paddingNumber + paddingUnit}));
 
   .image-text {
     text-align: center;
